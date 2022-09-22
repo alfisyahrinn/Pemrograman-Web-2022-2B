e83a2c811e8ebe74b0cbd084f4be93d1a17c20d9
alfisyahrinn <sahrin201@gmail.com>
2022-09-22 12:11:34 +0700

    update prak2

--- Praktikum2/4for.php
+++ Praktikum2/4for.php
@@ -15,22 +15,12 @@
   $kata = "belajar php";
   $huruf = "a";
   for ($i = 0; $i < strlen($kata); $i++) {
-    if ($udin = substr($kata, $i, 1 == $huruf)) {
+    if (substr($kata, $i, 1) == $huruf) {
       $jumlah++;
-      var_dump($udin);
     }
   }
   echo "Jumlah huruf " . $huruf . " dalam kata " . $kata . " : <br>";
   echo $jumlah;
-  echo "<br>";
-  echo "<br>";
-  echo "<br>";
-  echo "<br>";
-  echo "<br>";
-  echo "<br>"; 
-  echo substr($kata, 0, 1) . "<br>";
-  echo substr($kata, 1, 1) . "<br>";
-  echo substr($kata, 2, 1) . "<br>";
   ?>
 </body>
 
