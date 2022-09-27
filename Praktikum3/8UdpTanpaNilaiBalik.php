0e3ee859f5bb7f9bd4534b5873d1d22d6429bccf
alfisyahrinn <sahrin201@gmail.com>
2022-09-27 10:01:43 +0700

    nambabh prak3

---
+++ Praktikum3/8UdpTanpaNilaiBalik.php
@@ -0,0 +1,9 @@
+<?php
+function psgpjg ($pjg, $lbr) {
+$luas = $pjg * $lbr;
+echo "$luas <br>";
+}
+$bill = 5; $bil2 = 3;
+echo "Luas persegi panjang dengan pjg 5 dan lebar 3 =";
+psgpjg ($bill, $bil2);
+?>
