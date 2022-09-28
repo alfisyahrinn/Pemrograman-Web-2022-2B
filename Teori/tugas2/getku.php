d0d3ae183d35bbda77566f93624319f528e6f6e2
alfisyahrinn <sahrin201@gmail.com>
2022-09-28 17:29:25 +0700

    tugas 2 add

---
+++ Teori/tugas2/getku.php
@@ -0,0 +1,16 @@
+<?php
+$nama = $_GET["nama"];
+$noHp = $_GET["noHp"];
+$jk = $_GET["status"];
+$email = $_GET["email"];
+$tgl = $_GET["tgl"];
+$pw = $_GET["pw"];
+$statusku = $_GET["anjay"];
+
+echo "Nama : $nama <br>";
+echo "noHp : $noHp <br>";
+echo "jenis kelamin : $jk <br>";
+echo "email : $email <br>";
+echo "tanggal lahir : $tgl <br>";
+echo "password : $pw <br>";
+echo "status : $statusku <br>";
---
+++ Teori/tugas2/postku.php
@@ -0,0 +1,16 @@
+<?php
+$nama = $_POST["nama"];
+$noHp = $_POST["noHp"];
+$jk = $_POST["status"];
+$email = $_POST["email"];
+$tgl = $_POST["tgl"];
+$pw = $_POST["pw"];
+$statusku = $_POST["anjay"];
+
+echo "Nama : $nama <br>";
+echo "noHp : $noHp <br>";
+echo "jenis kelamin : $jk <br>";
+echo "email : $email <br>";
+echo "tanggal lahir : $tgl <br>";
+echo "password : $pw <br>";
+echo "status : $statusku <br>";
