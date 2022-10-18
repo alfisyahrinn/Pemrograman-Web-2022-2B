34abb90ca84404fa86f36cbfce73fdece8eaeb63
alfisyahrinn <sahrin201@gmail.com>
2022-10-18 12:20:52 +0700

    update home

--- Praktikum7/home.php
+++ Praktikum7/home.php
@@ -19,10 +19,14 @@ if (empty($_SESSION["username2b"])) {
 </head>
 
 <body>
-  <h1>Home ni bor</h1>
-  <a href="logout.php">
-    <button class="btn btn-danger">Logout</button>
-  </a>
+  <div style="height: 100vh; display: flex; justify-content: center; align-items: center;">
+    <div class="text-center">
+      <h1>INI HALAMAN HOME</h1>
+      <a href="logout.php">
+        <button class="btn btn-danger">Logout</button>
+      </a>
+    </div>
+  </div>
 
   <script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 </body>
